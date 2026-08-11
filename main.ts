e5958bc84774e0839c16f41de9994592ad2725aa
tipyman <149936594+tipyman@users.noreply.github.com>
2026-08-11 12:24:45 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
 namespace ext_irq {
     //% block="get medal count"
     //% shim=ext_irq::medalCountNative
-    export function getMedalCount(): number {
-        return 0; // shimでC++側を呼ぶので中身は不要
-    }
+    export function getMedalCount(): number;
 
     //% block="init irq"
     //% shim=ext_irq::initNative
-    export function init(): void {
-        return; // shimでC++側を呼ぶので中身は不要
-    }
+    export function init(): void;
 }
