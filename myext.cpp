1d58a1b5f583aa718ca2a7b3298a84fea9b8e4fa
tipyman <149936594+tipyman@users.noreply.github.com>
2026-08-11 12:05:44 +0900

    Update myext.cpp

--- myext.cpp
+++ myext.cpp
@@ -4,17 +4,18 @@ namespace ext_irq {
 
 static volatile int medalCount = 0;
 
-// 割り込みハンドラ（P0立ち上がり）
 void onP0Rise(MicroBitEvent e) {
     medalCount++;
 }
 
 //% block="get medal count native"
+//% shim=ext_irq::medalCountNative
 int medalCountNative() {
     return medalCount;
 }
 
 //% block="init irq native"
+//% shim=ext_irq::initNative
 void initNative() {
     uBit.messageBus.listen(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_RISE, onP0Rise);
 }
